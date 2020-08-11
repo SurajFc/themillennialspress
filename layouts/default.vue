8600f3efa2dc0ccdf58e7f41f50086b285082905
Suraj Thapa <surajthapafc@gmail.om>
2020-08-11 14:48:02 +0530

    (Removed) Social Sharea

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div class="container-fluid row">
-      <div class="col-lg-2 col-md-12 col-xs-12 card"></div>
-      <div class="col-lg-8 col-md-12 col-xs-12 col-sm-12">
+      <div class="col-md-2 col-sm-12"></div>
+      <div class="col-md-8 col-sm-12">
         <Navbar />
         <Nuxt />
       </div>
 
-      <div class="col-lg-2 col-md-12 col-xs-12 card"></div>
+      <div class="col-md-2 col-sm-12"></div>
     </div>
     <Footer />
   </div>
